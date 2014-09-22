cc70cdc1bf47afcec0a7afcb8c65b3f6fcf39d47
Ben Gotow <bengotow@gmail.com>
2014-09-22 09:33:40 -0700

    [h] Fix for includes

--- MModelCollection.h
+++ MModelCollection.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "MRestfulObject.h"
 #import "MModelCollectionFetcher.h"
-
+#import "MAPIClient.h"
 
 
 @class MModel;
--- MModelCollectionPaginatingFetcher.m
+++ MModelCollectionPaginatingFetcher.m
@@ -7,6 +7,7 @@
 //
 
 #import "MModelCollectionPaginatingFetcher.h"
+#import "MAPIClient.h"
 
 @implementation MModelCollectionPaginatingFetcher
 
